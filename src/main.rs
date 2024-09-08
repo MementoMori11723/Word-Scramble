879c4e976213fa9878c768139930a4a22bd31193
yasasvi <yasasvigumma@gmail.com>
2024-09-08 15:23:31 +0530

    First step of Guessing game
    looking pretty good!

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the number!");
+    println!("Enter your guess : ");
+    let mut guess = String::new();
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
+    println!("You guessed: {}", guess);
 }
